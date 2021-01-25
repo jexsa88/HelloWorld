975701081a11f2d857198e3c037cd190f8665692
jexsa88 <jexsa88@mail.ru>
2021-01-25 22:16:22 +0700

    пробник

--- app/src/main/java/com/example/helloworld/MainActivity.java
+++ app/src/main/java/com/example/helloworld/MainActivity.java
@@ -4,6 +4,9 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import java.net.SocketAddress;
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -14,5 +17,16 @@ public class MainActivity extends AppCompatActivity {
     public class Button {
         String but = "Кнопка";
         Integer but2 = 23;
+
     }
+    public class but1 {
+        Integer b1 = 21;
+    }
+    public class but2 {
+        Integer b2 = 2;
+    }
+    public class but3 {
+        Integer b3 = 33;
+    }
+
 }
