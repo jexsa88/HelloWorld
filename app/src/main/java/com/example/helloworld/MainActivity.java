27b973e6e982be71d722b8bb3741087b1f5ed305
jexsa88 <jexsa88@mail.ru>
2021-01-25 22:20:18 +0700

    фон

--- app/src/main/java/com/example/helloworld/MainActivity.java
+++ app/src/main/java/com/example/helloworld/MainActivity.java
@@ -14,19 +14,4 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-    public class Button {
-        String but = "Кнопка";
-        Integer but2 = 23;
-
-    }
-    public class but1 {
-        Integer b1 = 21;
-    }
-    public class but2 {
-        Integer b2 = 2;
-    }
-    public class but3 {
-        Integer b3 = 33;
-    }
-
 }
