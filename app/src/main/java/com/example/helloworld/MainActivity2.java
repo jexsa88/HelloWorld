bc0110b6015decaa37c2febde001c3e724e91042
jexsa88 <jexsa88@mail.ru>
2021-02-09 19:37:28 +0700

    Меню переработано, добавлено окно с песней цоя и кнопка назад

--- app/src/main/java/com/example/helloworld/MainActivity2.java
+++ app/src/main/java/com/example/helloworld/MainActivity2.java
@@ -5,10 +5,12 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 
 public class MainActivity2 extends AppCompatActivity implements View.OnClickListener{
     ImageView lyrics;
+    Button back;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,19 +19,32 @@ public class MainActivity2 extends AppCompatActivity implements View.OnClickList
 
         lyrics = (ImageView) findViewById(R.id.imageView5);
         ImageView imageView5 = (ImageView) findViewById(R.id.imageView5);
-
         imageView5.setOnClickListener(this);
+        back = (Button) findViewById(R.id.buttonback);
+        Button button1 = (Button) findViewById(R.id.buttonback);
+        button1.setOnClickListener(this);
     }
+
         public void onClick(View v) {
         switch (v.getId()) {
             case R.id.imageView5:
                 //Сделать
-                Intent intent = new Intent(this,MainActivity2.class);
+                Intent intent = new Intent(this,MainActivity3.class);
                 startActivity(intent);
                 break;
             default:
                 break;
         }
+        switch (v.getId()) {
+            case R.id.buttonback:
+                Intent intent = new Intent(this,MainActivity.class);
+                startActivity(intent);
+                break;
+            default:
+                break;
+        }
+
+
     }
 }
 
