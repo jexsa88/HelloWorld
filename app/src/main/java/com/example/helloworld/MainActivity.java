9c101c0da667dd0d05964e0c6aa816a35c892c8b
jexsa88 <jexsa88@mail.ru>
2021-02-09 14:29:53 +0700

    добавлено второе окно активности

--- app/src/main/java/com/example/helloworld/MainActivity.java
+++ app/src/main/java/com/example/helloworld/MainActivity.java
@@ -2,16 +2,34 @@ package com.example.helloworld;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import java.net.SocketAddress;
 import java.util.ArrayList;
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+    Button btnActTwo;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnActTwo = (Button) findViewById(R.id.btnActTwo);
+        btnActTwo.setOnClickListener(this);
+    }
+
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btnActTwo:
+                //Сделать
+                Intent intent = new Intent(this,MainActivity2.class);
+                startActivity(intent);
+                break;
+            default:
+                break;
+        }
     }
 }
