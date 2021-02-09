0ba0ceaf27a6cde5421e286e298fdc6ca9967d52
jexsa88 <jexsa88@mail.ru>
2021-02-09 17:08:49 +0700

    Добавлены альбомы и ссылки на них

--- app/src/main/java/com/example/helloworld/MainActivity.java
+++ app/src/main/java/com/example/helloworld/MainActivity.java
@@ -6,12 +6,21 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+
+import com.google.firebase.database.DatabaseReference;
 
 import java.net.SocketAddress;
 import java.util.ArrayList;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     Button btnActTwo;
+    private EditText mname;
+
+    private DatabaseReference mDataBase;
+    private String MUSIC_KEY = "Music";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,13 +28,32 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         btnActTwo = (Button) findViewById(R.id.btnActTwo);
         btnActTwo.setOnClickListener(this);
+//        init();
+
+
+    }
+
+//    public void init() {
+//        mname = findViewById(R.id.mname);
+//    }
+
+    public void onClickBtn2(View view) {
+
+    }
+
+    public void onClickBtn3(View view) {
+
+    }
+
+    public void onClickBtn4(View view) {
+
     }
 
     public void onClick(View v) {
         switch (v.getId()) {
             case R.id.btnActTwo:
                 //Сделать
-                Intent intent = new Intent(this,MainActivity2.class);
+                Intent intent = new Intent(this, MainActivity2.class);
                 startActivity(intent);
                 break;
             default:
--- app/src/main/java/com/example/helloworld/MainActivity2.java
+++ app/src/main/java/com/example/helloworld/MainActivity2.java
@@ -2,13 +2,34 @@ package com.example.helloworld;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
-public class MainActivity2 extends AppCompatActivity {
+public class MainActivity2 extends AppCompatActivity implements View.OnClickListener{
+    ImageView lyrics;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+
+        lyrics = (ImageView) findViewById(R.id.imageView5);
+        ImageView imageView5 = (ImageView) findViewById(R.id.imageView5);
+
+        imageView5.setOnClickListener(this);
+    }
+        public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.imageView5:
+                //Сделать
+                Intent intent = new Intent(this,MainActivity2.class);
+                startActivity(intent);
+                break;
+            default:
+                break;
+        }
     }
 }
+
